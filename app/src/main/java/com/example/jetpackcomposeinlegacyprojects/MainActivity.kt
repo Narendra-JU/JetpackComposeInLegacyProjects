a2d90b73e5a6096230c56f3b0b3fed7b183dd920
Narendra Nath Chatterjee <narendra.chatterjee>
2023-01-26 20:50:15 +0530

    Using Android ViewBinding in a compose project .

--- app/src/main/java/com/example/jetpackcomposeinlegacyprojects/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeinlegacyprojects/MainActivity.kt
@@ -7,12 +7,16 @@ import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.sp
+import androidx.compose.ui.viewinterop.AndroidViewBinding
 import com.example.jetpackcomposeinlegacyprojects.databinding.ActivityMainBinding
+import com.example.jetpackcomposeinlegacyprojects.databinding.XmlResourceFileBinding
 
 class MainActivity : AppCompatActivity() {
 
@@ -24,6 +28,7 @@ class MainActivity : AppCompatActivity() {
 
         binding.composeView.setContent {
             DemoComposeView()
+
         }
     }
 }
@@ -31,15 +36,22 @@ class MainActivity : AppCompatActivity() {
 @Preview
 @Composable
 fun DemoComposeView(){
+    var counter = remember {
+        mutableStateOf(0)
+    }
     Column(modifier = Modifier.fillMaxWidth(),
     horizontalAlignment = Alignment.CenterHorizontally,
     verticalArrangement = Arrangement.Top) {
         Text(
-
-            text = "This view is in compose",
+            text = "This view is in compose ${counter.value}",
             textAlign = TextAlign.Center,
             fontSize = 28.sp
-
         )
+
+        AndroidViewBinding(XmlResourceFileBinding::inflate){
+            this.button.setOnClickListener {
+                this.textView2.text = "Looks like this works ${++counter.value}"
+            }
+        }
     }
 }
