626e8381a8431036ee5927e8c9516f9ee30594ea
Narendra Nath Chatterjee <narendra.chatterjee>
2023-01-26 20:20:20 +0530

    Gradle setup for compose

--- app/src/main/java/com/example/jetpackcomposeinlegacyprojects/MainActivity.kt
+++ app/src/main/java/com/example/jetpackcomposeinlegacyprojects/MainActivity.kt
@@ -2,10 +2,44 @@ package com.example.jetpackcomposeinlegacyprojects
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.sp
+import com.example.jetpackcomposeinlegacyprojects.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.composeView.setContent {
+            DemoComposeView()
+        }
+    }
+}
+
+@Preview
+@Composable
+fun DemoComposeView(){
+    Column(modifier = Modifier.fillMaxWidth(),
+    horizontalAlignment = Alignment.CenterHorizontally,
+    verticalArrangement = Arrangement.Top) {
+        Text(
+
+            text = "This view is in compose",
+            textAlign = TextAlign.Center,
+            fontSize = 28.sp
+
+        )
     }
 }
